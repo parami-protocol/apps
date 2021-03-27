83c54baf2a33fbddd07f21d0535eb41fdeaf7d80
Andelf <andelf@gmail.com>
2021-03-27 13:21:33 +0800

    style(did-transfer): use transfer icon

--- packages/page-did-transfer/src/Transfer.tsx
+++ packages/page-did-transfer/src/Transfer.tsx
@@ -2,62 +2,49 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import BN from 'bn.js';
-import React, { useCallback, useState } from 'react';
-import styled from 'styled-components';
-import { Button, InputBalance, Input, TxButton } from '@polkadot/react-components';
-import { didToHex } from './util'
-import { useApi } from '@polkadot/react-hooks';
+import BN from "bn.js";
+import React, { useCallback, useState } from "react";
+import styled from "styled-components";
+import { Button, InputBalance, Input, TxButton } from "@polkadot/react-components";
+import { didToHex } from "./util";
+import { useApi } from "@polkadot/react-hooks";
 
 interface Props {
   className?: string;
   accountId?: string | null;
 }
 
-function DidTransfer ({ className, accountId }: Props): React.ReactElement<Props> {
+function DidTransfer({ className, accountId }: Props): React.ReactElement<Props> {
   const { api } = useApi();
-  const [amount, setAmount] = useState<BN | undefined | null>(new BN(10**15));
+  const [amount, setAmount] = useState<BN | undefined | null>(new BN(10 ** 15));
   const [did, setDid] = useState<string | null>(null);
   const [memo, setMemo] = useState<string | null>(null);
-  const _onDidChange = useCallback(
-    (did: string): void => {
-      const didHex = didToHex(did)
-      console.log(didHex, 'did hex----');
-      setDid(didHex)
-    },
-    []
-  );
+  const _onDidChange = useCallback((did: string): void => {
+    const didHex = didToHex(did);
+    console.log(didHex, "did hex----");
+    setDid(didHex);
+  }, []);
 
   return (
     <section className={className}>
-      <div className='ui--row'>
-        <div className='large'>
+      <div className="ui--row">
+        <div className="large">
           <h2>转账</h2>
           <Input
             maxLength={50}
             min={20}
             onChange={_onDidChange}
-            label='Recipient DID'
-            placeholder='请输入收款人的DID'
+            label="Recipient DID"
+            placeholder="请输入收款人的DID"
           />
-          <InputBalance
-            label='Amount to transfer'
-            onChange={setAmount}
-            value={amount}
-            placeholder='请输入转账金额'
-          />
-          <Input
-            maxLength={150}
-            onChange={setMemo}
-            label='Memo'
-            placeholder='请输入转账备注'
-          />
+          <InputBalance label="Amount to transfer" onChange={setAmount} value={amount} placeholder="请输入转账金额" />
+          <Input maxLength={150} onChange={setMemo} label="Memo" placeholder="请输入转账备注" />
           <Button.Group>
             <TxButton
               accountId={accountId}
               isDisabled={!amount || !did || !memo}
-              icon='check'
-              label='确认'
+              icon="paper-plane"
+              label="确认"
               params={[did, amount, memo]}
               tx={api.tx.did.transfer}
               withSpinner
